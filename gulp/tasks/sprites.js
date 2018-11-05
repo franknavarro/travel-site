df24733debb2e4978b306998861797b870a4901c
Frank <fnavarro7777@gmail.com>
2018-11-04 23:22:12 -0800

    Organized sprite automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,13 @@
 // This gulp action will take all the icons we have and create a spite file
 var gulp = require('gulp'),
-  svgSprite = require('gulp-svg-sprite');
+  svgSprite = require('gulp-svg-sprite'),
+  rename = require('gulp-rename');
 
+// Configuration for the gulp-svg-sprite package
 var config = {
   mode: {
     css: {
+      sprite: 'sprite.svg',
       render: {
         css: {
           template: './gulp/templates/sprite.css'
@@ -14,8 +17,24 @@ var config = {
   }
 }
 
+// Create sprite image and css for sprites
 gulp.task('createSprite', function(){
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+// Move the sprite image to our image folder
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+// Move sprite css to our modules folder for css
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteCSS', 'copySpriteGraphic']);
